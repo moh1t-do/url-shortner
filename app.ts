da0156b1345f774e3b0ec33759bc0b4bb9f84173
Mohit <mohitd2288@gmail.com>
2024-08-21 22:20:22 +0530

    chore: add boiler code

---
+++ app.ts
@@ -0,0 +1,14 @@
+import express, { Express, Request, Response, Application } from "express";
+import dotenv from "dotenv";
+
+//For env File
+dotenv.config();
+
+const app: Application = express();
+const port = process.env.PORT || 8000;
+
+app.get("/", (req: Request, res: Response) => {
+  res.send({ hey: "mom" });
+});
+
+app.listen(port);
