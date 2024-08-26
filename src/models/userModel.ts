23f73568591ed712f3bbefb034998632eaaead6c
Mohit <mohitd2288@gmail.com>
2024-08-26 20:53:11 +0530

    feat: add user model

---
+++ src/models/userModel.ts
@@ -0,0 +1,16 @@
+import { Schema, model, Model } from "mongoose";
+
+interface IUser {
+  name: string;
+  email: string;
+  password: string;
+}
+
+const userSchema = new Schema<IUser>({
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+});
+
+const User: Model<IUser> = model<IUser>("User", userSchema);
+export { User, IUser };
