d4f468d2f561acdf81b9d996c949c92db4afa5ab
Mohit <mohitd2288@gmail.com>
2024-09-05 18:54:06 +0530

    refactor:

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,17 @@
 import express, { Request, Response, Application } from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 import cookieParser from "cookie-parser";
 
 // routers
 import { router as heyRouter } from "./routes/heyRoute";
 import { router as authRouter } from "./routes/authRoute";
 import { router as shortUrlRouter } from "./routes/shortUrlRoute";
+import { router as refreshRouter } from "./routes/refreshRoute";
 
 // functions
 import { connectToDb } from "./connect";
-import { restrictToLoggedinuserOnly } from "./services/auth";
+import { verifyJWT } from "./middleware/authMiddleware";
 
 //For env File
 dotenv.config();
@@ -25,15 +27,22 @@ connectToDb(dbport);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true, // Allow credentials
+  })
+);
 
 app.get("/", (req: Request, res: Response) => {
   res.status(200).json({ working: true });
 });
 
 // all routes
-app.use("/api/v1/hey", restrictToLoggedinuserOnly, heyRouter);
+app.use("/api/v1/hey", verifyJWT, heyRouter);
 app.use("/api/v1/auth", authRouter);
-app.use("/api/v1/short", restrictToLoggedinuserOnly, shortUrlRouter);
+app.use("/api/v1/refresh", refreshRouter);
+app.use("/api/v1/short", verifyJWT, shortUrlRouter);
 
 // app.listen(port);
 app.listen(port, () => {
